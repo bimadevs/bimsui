77d66ba0eec3b082c613e0c439cc485c033cfb1e
bimaj0206 <bimaj0206@gmail.com>
2025-01-27 08:43:19 +0000

    add file txt

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,18 @@
+import { TextRevealCardPreview } from "../components/UI/reveal-card/demo";
+
+
+export default function Page() {
+    return (
+        <>
+            <div className="grid grid-cols-2 gap-2 h-[100vh]">
+                <div className="ml-2 w-full h-full px-4 flex justify-center items-center border-b">
+                    <h1 className="text-7xl">Design a website without the hassle
+                    </h1>
+                </div>
+                <div className="bg-green-500 w-full h-full"><TextRevealCardPreview /></div>
+                <div className="bg-blue-500 w-full h-full">s</div>
+                <div className="bg-gray-500 w-full h-full">s</div>
+            </div>
+        </>
+    )
+}
