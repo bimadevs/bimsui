b05bed889e753b67bba356f7cd91b09269e0b156
bimadevs <bimadev06@gmail.com>
2025-02-03 23:17:46 +0700

    add new image

--- src/app/pict/page.tsx
+++ src/app/pict/page.tsx
@@ -1,47 +1,13 @@
-import { TabsDemo } from "../components/nextjs/animate-tabs/demo";
-import { AnimatedDemo } from "../components/nextjs/animated-gradient-background/demo";
-import { AnimeNavBarDemo } from "../components/nextjs/anime-navbar/demo";
-import { Canvas } from "../components/nextjs/canvas/demo";
-import { CarouselDemo } from "../components/nextjs/carousel/demo";
-import { FloatingButton } from "../components/nextjs/floating-button/demo";
-import { GooeyTextDemo } from "../components/nextjs/goeeyText/demo";
-import { HeroParallaxDemo } from "../components/nextjs/hero-parallax/demo";
-import { Connect } from "../components/nextjs/highlighter/demo";
-import { InteraktifIcon } from "../components/nextjs/interaktif-icon/demo";
-import { SplineSceneBasic } from "../components/nextjs/interaktif3D/demo";
-import { LinkPreviewDemo } from "../components/nextjs/link-preview/demo";
-import { TextRevealCardPreview } from "../components/nextjs/reveal-card/demo";
-import { DemoHeroGeometric } from "../components/nextjs/shape-landing-hero/demo";
-import { ScrollaAnimation } from "../components/nextjs/scroll-animation/demo";
-import { TimelineDemo } from "../components/nextjs/timeline/demo";
-import { ThreeDCardDemo } from "../components/nextjs/3d-card-effect/demo";
-import { AnimatedTooltipPreview } from "../components/nextjs/animated-tooltip/demo";
-import { FloatingDockDemo } from "../components/nextjs/floating-dock/demo";
-import { LensDemo } from "../components/nextjs/lens/demo";
+'use client'
+import { NavbarOne, NavbarThree, NavbarTwo } from "../components/html/navbar";
+
 
 export default function pict() {
     return (
-        <div className="flex justify-center h-[100vh] items-center">
-        {/* <AnimeNavBarDemo /> */}
-        {/* <Canvas /> */}
-        {/* <FloatingButton /> */}
-        {/* <GooeyTextDemo /> */}
-        {/* <Connect /> */}
-        {/* <InteraktifIcon /> */}
-        {/* <SplineSceneBasic /> */}
-        {/* <ScrollaAnimation /> */}
-        {/* <TabsDemo /> */}
-        {/* <TextRevealCardPreview /> */}
-        {/* <HeroParallaxDemo /> */}
-        {/* <TimelineDemo /> */}
-        {/* <AnimatedDemo /> */}
-        {/* <CarouselDemo /> */}
-        {/* <LinkPreviewDemo /> */}
-        {/* <DemoHeroGeometric /> */}
-        {/* <ThreeDCardDemo /> */}
-        {/* <AnimatedTooltipPreview /> */}
-        {/* <FloatingDockDemo /> */}
-        <LensDemo />
+        <div className="px-32 flex  flex-col space-y-6 justify-center items-center gap-6 w-screen h-screen">
+        <NavbarOne />
+        <NavbarTwo />
+        <NavbarThree />
         </div>
     )
 }
